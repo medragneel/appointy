031215c5c1e43e85690401e6bfe53c95369a0e1c
mshadow <medmystgon@gmail.com>
2023-09-21 14:01:41 +0100

    v@1.1

--- Utility/gcal.js
+++ Utility/gcal.js
@@ -48,11 +48,8 @@ function authorize(credentials, callback) {
         client_id, client_secret, redirect_uris[0]);
 
     // Check if we have previously stored a token.
-    fs.readFile(TOKEN_PATH, (err, token) => {
-        if (err) return getAccessToken(oAuth2Client, callback);
-        oAuth2Client.setCredentials(JSON.parse(token));
+        oAuth2Client.setCredentials(JSON.parse(process.env.GOOGLE_CLIENT_TOKEN));
         callback(oAuth2Client);
-    });
 }
 
 /**
@@ -61,27 +58,27 @@ function authorize(credentials, callback) {
  * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
  * @param {function} callback The callback for the authorized client.
  */
-function getAccessToken(oAuth2Client, callback) {
-    const authUrl = oAuth2Client.generateAuthUrl({
-        access_type: 'offline',
-        scope: SCOPES,
-    });
-    console.log('Authorize this app by visiting this url:', authUrl);
-    const rl = readline.createInterface({input: process.stdin, output: process.stdout});
-    rl.question('Enter the code from that page here: ', (code) => {
-        rl.close();
-        oAuth2Client.getToken(code, (err, token) => {
-            if (err) return console.error('Error retrieving access token', err);
-            oAuth2Client.setCredentials(token);
-            // Store the token to disk for later program executions
-            fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-                if (err) return console.error(err);
-                console.log('Token stored to', TOKEN_PATH);
-            });
-            callback(oAuth2Client);
-        });
-    });
-}
+// function getAccessToken(oAuth2Client, callback) {
+//     const authUrl = oAuth2Client.generateAuthUrl({
+//         access_type: 'offline',
+//         scope: SCOPES,
+//     });
+//     console.log('Authorize this app by visiting this url:', authUrl);
+//     const rl = readline.createInterface({input: process.stdin, output: process.stdout});
+//     rl.question('Enter the code from that page here: ', (code) => {
+//         rl.close();
+//         oAuth2Client.getToken(code, (err, token) => {
+//             if (err) return console.error('Error retrieving access token', err);
+//             oAuth2Client.setCredentials(token);
+//             // Store the token to disk for later program executions
+//             fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+//                 if (err) return console.error(err);
+//                 console.log('Token stored to', TOKEN_PATH);
+//             });
+//             callback(oAuth2Client);
+//         });
+//     });
+// }
 
 module.exports = {
     SCOPES,
